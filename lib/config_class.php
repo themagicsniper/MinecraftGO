3c5c56896699488afb9abae46b28f2d69ce72e1f
themagicsniper <themagicsniper@gmail.com>
2012-10-12 20:14:45 -0500

    New config file

--- lib/config_class.php
+++ lib/config_class.php
@@ -1,6 +1,15 @@
 <?php
 	class Config
 	{
+		public $site_name = 'MinecraftGO';
+		public $site_url = 'localhost';
+		public $site_version = '0.1';
+
+		private $db_host = 'localhost';
+		private $db_user = 'root';
+		private $db_pass = 'pass';
+		private $db_name = 'mcGO';
+
 		public function __construct()
 		{
 			//CODE HERE
